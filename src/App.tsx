70c6b36823fcff1509e32ebb7262358bde876e0f
Amar Mahdi DAHMANI <amardahmani111@gmail.com>
2024-08-13 12:30:31 +0100

    first impression of the home page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Theme } from '@radix-ui/themes'
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  
+  return (
+    <>
+    <Theme>
+     <Home />
+     </Theme>
+    </>
+  )
+}
+
+export default App
